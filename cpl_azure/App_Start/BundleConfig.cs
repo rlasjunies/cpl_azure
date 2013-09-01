5e05305ac4d87370b26d87c4afd651649b804244
Richard Lasjunies <rlasjunies@gmail.com>
2013-09-01 13:19:38 +0200

    mvcupload integrated in the project

--- cpl_azure/App_Start/BundleConfig.cs
+++ cpl_azure/App_Start/BundleConfig.cs
@@ -9,51 +9,24 @@ namespace cpl_azure
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Scripts/jquery-ui-{version}.js",
                         "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/jquery.validate*", 
+                        "~/Scripts/bootstrap",
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryUploader").Include(
+                    "~/Scripts/jquery.fileupload.js",
+                    "~/Scripts/mvcfileupload/tmpl.min.js",
+                    "~/Scripts/mvcfileupload/mvcfileupload.js",
+                    "~/Scripts/mvcfileupload/jquery.fileupload-ui.js"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
-        
-            //jQueryDownloader
-            // Script Bundles
-            //BundleTable.Bundles.Add(new ScriptBundle("~/bundle/jquery").Include("~/Scripts/jquery-2.0.3.js"));
-            BundleTable.Bundles.Add(new ScriptBundle("~/Scripts/jquery-ui").Include("~/Scripts/jquery-ui-{version}.js"));
-            //BundleTable.Bundles.Add(new ScriptBundle("~/Scripts/jqueryUploader").Include("~/Scripts/jquery.fileupload.js"));
-            //BundleTable.Bundles.Add(new ScriptBundle("~/Scripts/jqueryUploaderIframe").Include("~/Scripts/jquery.iframe-transport.js"));
-            BundleTable.Bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/bootstrap.js"));
-
-
-            // Style Bundles
-            BundleTable.Bundles.Add(new StyleBundle("~/Styles/base").Include("~/Content/bootstrap.css",
-                "~/Content/jquery.fileupload-ui-noscript.css",
-               "~/Content/themes/base/jquery-ui*",
-               "~/Content/themes/base/jquery.ui*"));
-        
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/site.css",
+                        "~/Content/bootstrap.css",
+                        "~/Content/themes/base/jquery-ui.*",
+                        "~/Content/mvcfileupload/jquery.fileupload-bui.css"));        
         }
     }
 }
